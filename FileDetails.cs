823de405c9d3ec61b0d0e4c3dc106c0464e5117c
DipawaliNikam6 <89583557+DipawaliNikam6@users.noreply.github.com>
2021-08-26 17:31:22 +0530

    Update FileDetails.cs

--- FileDetails.cs
+++ FileDetails.cs
@@ -1,1 +1,30 @@
+using System;
+using System.IO;
+using System.Diagnostics;
 
+Class FileDetails
+{
+ 
+public string? FileVersion { get; }
+
+public int GetVersion(String FileName)  
+{  
+        FileVersionInfo.GetVersionInfo(Path.Combine(Environment.SystemDirectory, FileName));
+        FileVersionInfo myFileVersionInfo = FileVersionInfo.GetVersionInfo(Environment.SystemDirectory + FileName);
+        FileVersion=myFileVersionInfo.FileVersion
+        return FileVersion
+        // Print the file name and version number.
+        Console.WriteLine("File: " + myFileVersionInfo.FileDescription + '\n' +
+           "Version number: " + myFileVersionInfo.FileVersion);
+    
+}
+public int GetSize(String file)  
+{  
+
+// Get the information about a file
+FileInfo fi = new FileInfo(file);
+// Print the file size to console
+Console.WriteLine($"File size: {fi.Length} bytes");
+}
+
+}
